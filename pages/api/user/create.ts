54ef431ef6bd8e858d0f8cd828ce2e2465851a25
Anurag30112003 <anuragsharma011011@gmail.com>
2022-12-25 03:23:41 +0530

    login function and added types

--- pages/api/user/create.ts
+++ pages/api/user/create.ts
@@ -3,8 +3,16 @@ import { prisma } from "../../../db/client";
 const bcrypt = require("bcryptjs");
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-  const { name, email, password, password2 } = req.body;
-  const exists: Promise<Array<{ id: number }>> = prisma.user.findMany({ where: { email } });
+  const {
+    name,
+    email,
+    password,
+    password2,
+  }: { name: string; email: string; password: string; password2: string } =
+    req.body;
+  const exists: Promise<Array<{ id: number }>> = prisma.user.findMany({
+    where: { email },
+  });
   if (!name || !email || !password || !password2) {
     res.status(400).json("Please enter all fields");
   } else if ((await exists).length > 0) {
@@ -13,12 +21,12 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
     res.status(400).json("Passwords do not match");
   } else {
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword:string = await bcrypt.hash(password, salt);
+    const hashedPassword: string = await bcrypt.hash(password, salt);
     const user = await prisma.user.create({
       data: {
         name,
         email,
-        password : hashedPassword,
+        password: hashedPassword,
       },
     });
     res.status(200).json(user);
